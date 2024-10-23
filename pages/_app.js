d244865624816ae8fb3a87baea36eccbe8b240a5
Ashish Verma <opr1004@infarmbureau.com>
2024-10-23 00:47:19 -0400

    base project setup

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// pages/_app.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/global.scss';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
